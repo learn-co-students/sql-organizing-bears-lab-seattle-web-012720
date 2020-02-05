97e977f827ad4999a428679eb3be813fda45f14e
github username <github email address>
2020-02-05 13:52:04 -0800

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+create table bears (
+   id INTEGER PRIMARY KEY,
+   name TEXT,
+   age INTEGER,
+   gender TEXT,
+   color TEXT,
+   temperament TEXT,
+   alive BOOLEAN);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'Male', 'Brown', 'Wild', "Y"),
+('Rowdy', 10, 'Male', 'Brown', 'Wild', "Y"),
+('Tabitha', 10, 'Female', 'Black', 'Bucknasty', "Y"),
+('Sergeant Brown', 10, 'Male', 'Brown', 'Dead', "N"),
+('Melissa', 10, 'Female', 'Polar', 'Tame', "Y"),
+('Grinch', 10, 'Male', 'Brown', 'Wild', "N"),
+('Wendy', 10, 'Female', 'Brown', 'Wild', "Y"),
+(null, 10, 'Male', 'Clear', 'Murderer', "Y");
